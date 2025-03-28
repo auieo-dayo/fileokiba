9de3f2453e23d0f9f14f335162e3419ee802afc4
auieo-dayo <yud.nitayama@gmail.com>
2025-03-28 04:15:28 +0000

    a

--- iroiro.js
+++ iroiro.js
@@ -76,3 +76,30 @@ const nowtime = {
   function random_int(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a; // 修正: b - a + 1 に変更
 }
+//cookie
+const CookieManager = {
+  set: (name, value, days = 7) => {
+    let expires = "";
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = `; expires=${date.toUTCString()}`;
+    }
+    document.cookie = `${name}=${encodeURIComponent(value)}${expires}; path=/`;
+  },
+
+  get: (name) => {
+    const nameEQ = `${name}=`;
+    const cookies = document.cookie.split('; ');
+    for (let cookie of cookies) {
+      if (cookie.startsWith(nameEQ)) {
+        return decodeURIComponent(cookie.substring(nameEQ.length));
+      }
+    }
+    return null;
+  },
+
+  delete: (name) => {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+  }
+}
